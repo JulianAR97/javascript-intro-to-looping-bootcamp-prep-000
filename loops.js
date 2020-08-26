1a77e330075dec5aec1ded42f52bf3fa72e4b1ae
whimsical-logger-3153 <julianrosenthal@yahoo.com>
2020-08-26 00:02:51 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,1 +1,24 @@
+function forLoop(arr) {
+  for (var i = 0; i < 25; i++) {
+    if (i === 1) {
+      arr.push('I am 1 strange loop.');
+    } else {
+      arr.push(`I am ${i} strange loops.`)
+    }
+  }
+  return arr;
+}
 
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(n);
+    n--;
+  }
+  return 'done';
+}
+
+function doWhileLoop(num) {
+  do {
+    'I run once regardless.'
+  } while (num++ < 10);
+}
